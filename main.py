ea083ed6ea41b546439187dc180cd3217cb9e553
Marius <mariusblees@gmail.com>
2023-04-12 10:57:34 +0200

    added check_color_win function

--- main.py
+++ main.py
@@ -35,6 +35,119 @@ class Board:
                 print(" ", end="")
             print()
 
+    def check_color_win(self, color):
+        # Check rows
+        for x in range(4):
+            for y in range(4):
+                for z in range(1):  # Only need to check first layer of depth
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x][y][z + 1].get_color() == color and
+                            self.grid[x][y][z + 2].get_color() == color and
+                            self.grid[x][y][z + 3].get_color() == color):
+                        return True
+
+        # Check columns
+        for x in range(4):
+            for y in range(1):  # Only need to check first layer of height
+                for z in range(4):
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x][y + 1][z].get_color() == color and
+                            self.grid[x][y + 2][z].get_color() == color and
+                            self.grid[x][y + 3][z].get_color() == color):
+                        return True
+
+        # Check depth
+        for x in range(1):  # Only need to check first layer of width
+            for y in range(4):
+                for z in range(4):
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x + 1][y][z].get_color() == color and
+                            self.grid[x + 2][y][z].get_color() == color and
+                            self.grid[x + 3][y][z].get_color() == color):
+                        return True
+
+# Check diagonals
+        for x in range(1):  # Only need to check first layer of width
+            for y in range(1):  # Only need to check first layer of height
+                for z in range(4):
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x + 1][y + 1][z].get_color() == color and
+                            self.grid[x + 2][y + 2][z].get_color() == color and
+                            self.grid[x + 3][y + 3][z].get_color() == color):
+                        return True
+
+        for x in range(1):  # Only need to check first layer of width
+            for y in range(3, 4):
+                for z in range(4):
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x + 1][y - 1][z].get_color() == color and
+                            self.grid[x + 2][y - 2][z].get_color() == color and
+                            self.grid[x + 3][y - 3][z].get_color() == color):
+                        return True
+
+        for x in range(4):
+            for y in range(1):  # Only need to check first layer of height
+                for z in range(1):  # Only need to check first layer of depth
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x - 1][y + 1][z + 1].get_color() == color and
+                            self.grid[x - 2][y + 2][z + 2].get_color() == color and
+                            self.grid[x - 3][y + 3][z + 3].get_color() == color):
+                        return True
+
+        for x in range(4):
+            for y in range(1):  # Only need to check first layer of height
+                for z in range(3, 4):
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x - 1][y + 1][z - 1].get_color() == color and
+                            self.grid[x - 2][y + 2][z - 2].get_color() == color and
+                            self.grid[x - 3][y + 3][z - 3].get_color() == color):
+                        return True
+
+        for x in range(4):
+            for y in range(4):
+                for z in range(1):  # Only need to check first layer of depth
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x - 1][y][z + 1].get_color() == color and
+                            self.grid[x - 2][y][z + 2].get_color() == color and
+                            self.grid[x - 3][y][z + 3].get_color() == color):
+                        return True
+
+        for x in range(1):  # Only need to check first layer of width
+            for y in range(1):  # Only need to check first layer of height
+                for z in range(1):  # Only need to check first layer of depth
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x + 1][y + 1][z].get_color() == color and
+                            self.grid[x + 2][y + 2][z].get_color() == color and
+                            self.grid[x + 3][y + 3][z].get_color() == color):
+                        return True
+
+        for x in range(3, 4):  # Only need to check last layer of width
+            for y in range(1):  # Only need to check first layer of height
+                for z in range(1):  # Only need to check first layer of depth
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x - 1][y + 1][z].get_color() == color and
+                            self.grid[x - 2][y + 2][z].get_color() == color and
+                            self.grid[x - 3][y + 3][z].get_color() == color):
+                        return True
+
+        for x in range(1):  # Only need to check first layer of width
+            for y in range(1):  # Only need to check first layer of height
+                for z in range(4):
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x + 1][y + 1][z + 1].get_color() == color and
+                            self.grid[x + 2][y + 2][z + 2].get_color() == color and
+                            self.grid[x + 3][y + 3][z + 3].get_color() == color):
+                        return True
+
+        for x in range(3, 4):  # Only need to check last layer of width
+            for y in range(1):  # Only need to check first layer of height
+                for z in range(3, 4):  # Only need to check last layer of depth
+                    if (self.grid[x][y][z].get_color() == color and
+                            self.grid[x - 1][y + 1][z - 1].get_color() == color and
+                            self.grid[x - 2][y + 2][z - 2].get_color() == color and
+                            self.grid[x - 3][y + 3][z - 3].get_color() == color):
+                        return True
+
 
 board = Board()
 board.set_point_color(0, 0, 0, 'x')
@@ -42,3 +155,5 @@ board.set_point_color(1, 1, 1, 'x')
 board.set_point_color(2, 2, 2, 'x')
 board.set_point_color(3, 3, 3, 'x')
 board.print_board()
+if board.check_color_win('x'):
+    print("x has won")
